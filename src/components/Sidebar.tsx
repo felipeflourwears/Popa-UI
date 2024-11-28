d58a50990cfe0324ea5000c14cc88fb7052165a8
felipeflourwears <A01274880@tec.mx>
2024-11-27 22:23:44 -0600

    Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <div className="bg-neutral-900 w-60 p-3 flex flex-col text-white">
+      <div className="flex-1">Top Part</div>
+      <div>Bottom Part</div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../Sidebar";
 
 const Layout = () => {
   return (
-    <div>
-      <div className="bg-indigo-500">Sidebar</div>
-      <div className="bg-indigo-300">Header</div>
-      <div className="">{<Outlet />}</div>
-      <p>footer</p>
+    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+      <Sidebar/>
+      <div className="p-4">
+        <div className="bg-indigo-300">Header</div>
+        <div className="">{<Outlet />}</div>
+      </div> 
     </div>
   );
 };
