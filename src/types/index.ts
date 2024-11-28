4c8562c2a9694eac652d8d556df0cfe17742a04b
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2024-11-28 13:13:36 -0600

    Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,9 +7,17 @@ export type SidebarLinkType = {
   icon: ReactNode;
 };
 
-export type Selector = {
-  id: string
-  deviceType: { value: string; label: string };
-}
+export type DeviceOption = {
+  value: string;
+  label: string;
+};
 
-export type DraftSelector = Omit<Selector, 'id'>
+export type DeviceMap = Record<string, DeviceOption[]>;
+
+export type DraftSelector = {
+  deviceType: DeviceOption | null;
+  device: DeviceOption | null;
+  nombreVideo: File | null; // Cambiamos el tipo a File
+  colorRGB: string | null;
+};
+
