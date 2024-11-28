75891a58e8e083b3daf810c92f68376232cb3040
felipeflourwears <A01274880@tec.mx>
2024-11-27 23:02:14 -0600

    Sidebar add styles with tailwind and types about sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,35 @@
+import { FcDatabase } from "react-icons/fc"
+import { DASHBOARD_SIDEBAR_LINKS } from "../lib/consts/navigation"
+import { Link } from "react-router-dom"
+import { SidebarLinkType } from "../types"
+
+const linkClass =
+	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+
 const Sidebar = () => {
   return (
     <div className="bg-neutral-900 w-60 p-3 flex flex-col text-white">
-      <div className="flex-1">Top Part</div>
+      <div className="flex items-center gap-2 px-1 py-3">
+        <FcDatabase fontSize={24}/>
+        <span className="text-neutral-100 text-lg">Popa UI</span>
+      </div>
+      <div className="flex-1">
+        {DASHBOARD_SIDEBAR_LINKS.map((item)=>(
+            <SidebarLink key={item.key} item={item}/>
+        ))}
+      </div>
       <div>Bottom Part</div>
     </div>
   )
+}
+
+function SidebarLink({item} : { item: SidebarLinkType }){
+    return(
+        <Link to={item.path} className={linkClass}>
+            <span className="text-xl">{item.icon}</span>
+            {item.label}
+        </Link>
+    )
 }
 
 export default Sidebar
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+
+export type SidebarLinkType = {
+  key: string;
+  label: string;
+  path: string;
+  icon: ReactNode;
+};
